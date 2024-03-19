30b963ca531ad7921fb9967682eb314740196c0a
kesava1234567890 <121613456+kesava1234567890@users.noreply.github.com>
2024-03-19 21:45:20 +0530

    Delete 9.py

--- 9.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import numpy as np
-df=pd.read_excel(r"C:/Users/leela/OneDrive/Desktop/QPDS/sales-data.xlsx")
-print(pd.pivot_table(df,index=["Region","Manager","SalesMan"],values="Sale_amt",aggfunc=np.sum))
